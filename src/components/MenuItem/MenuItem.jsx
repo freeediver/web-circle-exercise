0483bc97ff44c118c6f90a7064430b6fa1be8645
freeediver <cr_kherfallah@esi.dz>
2026-01-23 21:34:40 +0100

    adding wishlist functionality  without real actions - just interface  (to be continued)

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -4,14 +4,31 @@ import styles from "./MenuItem.module.css";
 
 const MenuItem = ({ dish }) => {
   const { strMeal: name, strMealThumb: image } = dish;
+  const isWishlisted =false;
+  
   const navigate = useNavigate();
-  return (
+   return (
     <div className={styles.menuItem}>
       <h3>{name}</h3>
       <img src={image} alt={name} />
+      
+      <div className={styles.wishlistIcon} onClick="">
+        {isWishlisted ? (
+          <span title="Remove from wishlist">❤️</span>
+        ) : (
+          <span title="Add to wishlist">🤍</span>
+        )}
+      </div>
+      
       <div className={styles.menuItemBtnContainer}>
         <Button onClick={() => navigate(`/meals/${dish.idMeal}`)}>
           Details
+        </Button>
+        <Button 
+          onClick=""
+          variant={isWishlisted ? "secondary" : "primary"}
+        >
+          {isWishlisted ? "Remove from Wishlist" : "Add to Wishlist"}
         </Button>
       </div>
     </div>
